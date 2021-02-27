9b27b8507fda4f12330439dffb31065f2d75f749
atrox3d <atrox3d@gmail.com>
2021-02-27 05:49:59 +0100

    update google sheet from nutritionix via sheety

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
 import myob
 import requests
 import json
+from datetime import datetime as dt
 
-APP_ID = myob.APP_ID
-APP_KEY = myob.APP_KEY
-ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+NUTRITIONIX_APPID = myob.NUTRITIONIX_APPID
+NUTRITIONIX_APPKEY = myob.NUTRITIONIX_APPKEY
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-HEADERS = {
-    "x-app-id": APP_ID,
-    "x-app-key": APP_KEY
-}
+SHEETY_USER = myob.SHEETY_USER
+SHEETY_PROJECT = myob.SHEETY_PROJECT
+SHEETY_SHEET = myob.SHEETY_SHEET
+SHEETY_ENDPOINT = f"https://api.sheety.co/{SHEETY_USER}/{SHEETY_PROJECT}/{SHEETY_SHEET}"
 
-params = {
-    # "query": "ran 3 miles",
-    "query": "swam 40 min and ran 3km and 30 min pushups",
-    "gender": "male",
-    "weight_kg": 98,
-    "height_cm": 170.0,
-    "age": 50
-}
 
-# query = input("Tell me which exercises you did: ")
+def query_nutritionix(query: str = None) -> requests.models.Response:
+    headers = {
+        "x-app-id": NUTRITIONIX_APPID,
+        "x-app-key": NUTRITIONIX_APPKEY
+    }
 
-# params["query"] = query
+    params = {
+        # "query": "ran 3 miles",
+        "query": "swam 40 min and ran 3km and 30 min pushups",
+        "gender": "male",
+        "weight_kg": 98,
+        "height_cm": 170.0,
+        "age": 50
+    }
+    if query:
+        params["query"] = query
 
-response = requests.post(url=ENDPOINT, json=params, headers=HEADERS)
-print(response.status_code)
-print(
-    json.dumps(response.json(), indent=4)
-)
+    response = requests.post(url=NUTRITIONIX_ENDPOINT, json=params, headers=headers)
+    print(response.status_code)
+    # print(type(response))
+    # data = response.json()
+    # print(type(data))
+    # print(json.dumps(data, indent=4))
+    return response
+
+
+def update_sheety(exercise: dict) -> requests.models.Response:
+    # print(json.dumps(exercise, indent=4))
+    payload = {
+        "workout": {
+            "date": dt.strftime(dt.now(), "%Y%m%d"),
+            "time": dt.strftime(dt.now(), "%H:%M:%S %p"),
+            "exercise": exercise["name"],
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+    print(payload)
+    response = requests.post(SHEETY_ENDPOINT, json=payload)
+    return response
+
+
+################################################################################
+nutritionix_response = query_nutritionix()
+nutritionix_data = nutritionix_response.json()
+
+for exercise in nutritionix_data["exercises"]:
+    response = update_sheety(exercise)
+    print(response.status_code)
+    print(response.text)
